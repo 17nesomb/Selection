4224ab55c76f876b543e5838d74d84385333d22f
Benjamin Nesom <17NesomB@fulford.york.sch.uk>
2022-09-14 11:52:07 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your favourite pizza? ");
+            string pizzaFlavour = Console.ReadLine();
+            pizzaFlavour = pizzaFlavour.ToLower();
+
+            if (pizzaFlavour == "cheese and tomato" || pizzaFlavour == "margherita")
+            {
+                Console.WriteLine("Suitable for vegetarians");
+            }
+
+            else if(pizzaFlavour == "pepperoni" || pizzaFlavour == "meat feast")
+            {
+                Console.WriteLine("Contains Meat");
+            }
+
+            else
+            {
+                Console.WriteLine("Unknown pizza, may contain meat");
+            }
+                
+        }
+    }
+}
